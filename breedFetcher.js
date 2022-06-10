07eeccd0f679b080930c9b8f527f419822fa169f
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-06-10 00:01:30 +0000

    refractored the function, now the user can input any breedname into the command line and retrieve a description

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,22 @@
 
 const request = require('request');
 
-request('https://api.thecatapi.com/v1/breeds/search?q=Ben', (error, response, body) => {
-  // sad path
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-  }// console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //happy path
-  const data = JSON.parse(body); //parse will take JSON as a sting and make into object!
-  console.log('body:', data[0].description); //which is now an object , Access the first entry in the data array and print out the description for the user.
-});
+const fetchBreedDescription = function(breedName, callback) {
 
-/* during error testing error: Error: getaddrinfo ENOTFOUND api.thecat345api.com
-    at GetAddrInfoReqWrap.onlookup [as oncomplete] (node:dns:71:26) {
-      errno: -3008,
-      code: 'ENOTFOUND',  ==> means No Entity, no such place
-      syscall: 'getaddrinfo',
-      hostname: 'api.thecat345api.com'
-}*/
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    //sad path
+    if (error) {
+      return callback(error,null); 
+    } 
+    //happy path
+      const data = JSON.parse(body); //parse will take JSON as a sting and make into object!  
+      if(callback) {
+        callback(error,data[0].description);
+      }
+  });
+};
 
 
 
+
+module.exports = {fetchBreedDescription}
